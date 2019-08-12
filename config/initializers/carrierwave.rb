74c478dcf554d75dc3b3d86940b84722a2f8b91e
Bre Phelps <bphelps@live.com>
2019-08-12 02:22:51 +0000

    image

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,11 @@
 CarrierWave.configure do |config|
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+
   config.fog_provider = 'fog/aws'                        # required
+  config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
