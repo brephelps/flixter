52501c9f952676956fdc16b845da4a47fec4c310
Bre Phelps <bphelps@live.com>
2019-08-15 00:54:12 +0000

    enrollment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,9 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :enrolled_in?
+  # has_many :enrolled_users, through: :enrollments, source: :users
+  before_action :require_enrolled_users, only: [:show]
+
   def show
   end
 
@@ -7,5 +12,15 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def course
+    @course = current_lesson.section.course
+  end
+
+  def require_enrolled_users
+    if !current_user.enrolled_in?(course)
+      redirect_to course_path(course), alert: 'You must be enrolled in this Course in order to view the Lessons.'
+    end
   end
 end
