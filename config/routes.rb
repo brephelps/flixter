be54e02a307aa3f2523a66caca3db957c084c942
Bre Phelps <bphelps@live.com>
2019-08-18 20:22:13 +0000

    final touches

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
 
   resources :users, only: :show
   resources :courses, only: [:index, :show] do
